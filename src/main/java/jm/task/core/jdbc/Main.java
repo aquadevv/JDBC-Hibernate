43df260f860c67abf65cf2e7e4d70272bd068472
Ворончихин Иван Романович <bonoberkut@gmail.com>
2025-01-23 21:16:03 +0300

    task113-jdbc Add user creation and logging functionality in Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final UserServiceImpl userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 5);
+        userService.saveUser("Petr", "Petrov", (byte) 10);
+        userService.saveUser("Sidor", "Sidorov", (byte) 15);
+        userService.saveUser("Vasiliy", "Vasiliev", (byte) 20);
+        userService.getAllUsers().forEach(user -> logger.info(user.toString()));
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }

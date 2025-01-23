83d8f3f08eb4b9d1aebaaa03d6e99243191cdcf0
Ворончихин Иван Романович <bonoberkut@gmail.com>
2025-01-23 21:14:12 +0300

    task113-jdbc Add toString method to User class

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -22,6 +22,11 @@ public class User {
 
     }
 
+    @Override
+    public String toString() {
+        return String.format("User with name - %s added to the database", name);
+    }
+
     public User(String name, String lastName, Byte age) {
         this.name = name;
         this.lastName = lastName;
